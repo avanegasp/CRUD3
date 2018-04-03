84eeadd5de5f1dda353b156ff0f0cca43fa0a08d
avanegasp <arco527@gmail.com>
2018-04-02 22:02:52 -0500

    Ready crud but need css

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -20,6 +20,32 @@ end
     @notes = Note.find(params[:id])
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    if @note.update(params_notes)
+      flash[:notice] = 'note update!'
+      redirect_to notes_path
+    else
+      flash[:error] = 'failed to edit note'
+      render :edit
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+      if @note.delete
+        flash[:notice]
+        redirect_to notes_path
+      else
+        flash[:error]
+        render :destroy
+      end
+  end
+
   private
   def params_notes
     params.require(:note).permit(:title,:body).merge(user_id: current_user.id)
